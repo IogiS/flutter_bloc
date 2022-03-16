1a0a7ee6120185a39390ef00f698f783a25e388c
Løg!SXXX <31819845+IogiS@users.noreply.github.com>
2022-03-17 02:21:54 +0300

    Update first_page.dart

--- flutter_shop/lib/main_pages/first_page.dart
+++ flutter_shop/lib/main_pages/first_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_shop/bloc/albums_bloc.dart';
 import 'package:lazy_load_scrollview/lazy_load_scrollview.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:refresh_loadmore/refresh_loadmore.dart';
 
 class FirstPage extends StatefulWidget {
@@ -35,75 +36,13 @@ class _FirstPageState extends State<FirstPage> {
     return Scaffold(
         body: BlocListener<AlbumsBloc, AlbumsState>(
             listener: (context, AlbumsState state) {
-      if (state is AlbumsUpdated) {
-        print('obj');
-      }
+      if (state is AlbumsUpdated) {}
     }, child: BlocBuilder<AlbumsBloc, AlbumsState>(
       builder: (context, state) {
         if (state is AlbumsInitial) {
           return const CircularProgressIndicator(color: Colors.blue);
         }
-        return SingleChildScrollView(
-            child: SizedBox(
-                height: MediaQuery.of(context).size.height - 150,
-                child: LazyLoadScrollView(
-                    onEndOfPage: () => BlocProvider.of<AlbumsBloc>(context)
-                        .add(LazyLoadAlbums()),
-                    child: RefreshIndicator(
-                        onRefresh: _refresh,
-                        child: ListView.builder(
-                            padding: const EdgeInsets.all(8),
-                            itemCount: BlocProvider.of<AlbumsBloc>(context)
-                                .albums
-                                .length,
-                            itemBuilder: (BuildContext context, int index) {
-                              if (index ==
-                                  BlocProvider.of<AlbumsBloc>(context)
-                                          .albums
-                                          .length -
-                                      1) {
-                                return const Center(
-                                    child: CircularProgressIndicator());
-                              } else {
-                                return Center(
-                                  child: Card(
-                                    child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        ListTile(
-                                          leading: const Icon(Icons.album),
-                                          subtitle: Text(
-                                              BlocProvider.of<AlbumsBloc>(
-                                                      context)
-                                                  .albums[index]['title']
-                                                  .toString()),
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          children: <Widget>[
-                                            TextButton(
-                                              child: Text(
-                                                  BlocProvider.of<AlbumsBloc>(
-                                                          context)
-                                                      .albums[index]['id']
-                                                      .toString()),
-                                              onPressed: () {/* ... */},
-                                            ),
-                                            const SizedBox(width: 8),
-                                            TextButton(
-                                              child: const Text('LISTEN'),
-                                              onPressed: () {/* ... */},
-                                            ),
-                                            const SizedBox(width: 8),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                );
-                              }
-                            })))));
+        return cardList();
       },
     )));
   }
@@ -111,5 +50,80 @@ class _FirstPageState extends State<FirstPage> {
   Future<void> _refresh() {
     BlocProvider.of<AlbumsBloc>(context).add(LoadAlbums());
     return Future.delayed(Duration(seconds: 0, milliseconds: 2000));
+  }
+
+  Widget card(int index) {
+    return GestureDetector(
+        onTap: () => Navigator.push(
+              context,
+              PageTransition(
+                  type: PageTransitionType.fade,
+                  child: Scaffold(
+                      appBar: AppBar(
+                        title: const Text('card'),
+                      ),
+                      body: card(index)),
+                  inheritTheme: true,
+                  ctx: context),
+            ),
+        child: Center(
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  leading: const Icon(Icons.album),
+                  subtitle: Text(BlocProvider.of<AlbumsBloc>(context)
+                      .albums[index]['title']
+                      .toString()),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: Text(BlocProvider.of<AlbumsBloc>(context)
+                          .albums[index]['id']
+                          .toString()),
+                      onPressed: () {/* ... */},
+                    ),
+                    const SizedBox(width: 8),
+                    TextButton(
+                      child: const Text('LISTEN'),
+                      onPressed: () {/* ... */},
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget cardList() {
+    return SingleChildScrollView(
+        child: SizedBox(
+            height: MediaQuery.of(context).size.height - 150,
+            child: LazyLoadScrollView(
+                onEndOfPage: () =>
+                    BlocProvider.of<AlbumsBloc>(context).add(LazyLoadAlbums()),
+                child: RefreshIndicator(
+                    onRefresh: _refresh,
+                    child: ListView.builder(
+                        padding: const EdgeInsets.all(8),
+                        itemCount:
+                            BlocProvider.of<AlbumsBloc>(context).albums.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          if (index ==
+                              BlocProvider.of<AlbumsBloc>(context)
+                                      .albums
+                                      .length -
+                                  1) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          } else {
+                            return card(index);
+                          }
+                        })))));
   }
 }
